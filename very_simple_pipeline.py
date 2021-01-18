aeb3403bb9da90ded0d64cb39e4146b311dff8e4
Marc Klinger <mklinger@mklinger.de>
2021-01-18 18:26:57 +0100

    Prepare adding more text sources

--- very_simple_pipeline.py
+++ very_simple_pipeline.py
@@ -1,34 +1,43 @@
 
-from scanstotext.StaccaredProject import File, PdfInput, Text, TextBlock, StaccaredProject
+from scanstotext.StaccaredProject import PdfInput
 from optparse import OptionParser
 import string
 import random
 import json
 
+
 def main():
     input_file = get_opts()
     file_id = get_random_string(8)
 
-    file = {
-        "filename": input_file,
-        "file_id": file_id
-    }
+    texts = []
 
     # Currently we support only one pdf-file
-    loader = PdfInput(input_file)
-    text_blocks = []
-    for page_no in range(1, loader.get_number_of_pages() + 1):
-        page_text = loader.get_page_text(page_no);
-        text_blocks.append({
-            "page": page_no,
-            "text": page_text
-        })
+    pdf_texts = extract_pdf_texts(input_file)
+    texts.extend(pdf_texts)
 
-    file["text_blocks"] = text_blocks;
+    file = {
+        "filename": input_file,
+        "file_id": file_id,
+        "texts": texts
+    }
 
     result = [file]
 
     print(json.dumps(result, indent=2, ensure_ascii=False))
+
+
+def extract_pdf_texts(input_file):
+    with PdfInput(input_file) as loader:
+        pdf_texts = []
+        for page_no in range(1, loader.get_number_of_pages() + 1):
+            pdf_text = loader.get_page_text(page_no)
+            pdf_texts.append({
+                "page": page_no,
+                "source": "PDF_TEXT",
+                "text": pdf_text
+            })
+        return pdf_texts
 
 
 def get_opts():
@@ -38,7 +47,7 @@ def get_opts():
     if len(args) < 1:
         parser.error("Incorrect number of arguments")
 
-    input_file = args[0];
+    input_file = args[0]
 
     return input_file
 
